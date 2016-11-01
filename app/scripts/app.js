9694aaa8e8822744c2f7ecbeaca490a557bef24a
lena95 <lenka050695@mail.ru>
2016-11-02 01:02:36 +0400

    comm1

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name todoManagerApp
+ * @description
+ * # todoManagerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('todoManagerApp', [
+    'ngAria',
+    'ngMessages'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoManagerApp
+ */
+angular.module('todoManagerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
